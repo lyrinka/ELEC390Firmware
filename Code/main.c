05cd87a551bdbeca3b335a814a6fbdd850ef63b6
Lyrinka Hyacinthoides <47382398+lyrinka@users.noreply.github.com>
2023-07-15 03:04:21 +0800

    Test code
    Horrible framework, just for testing

--- Code/main.c
+++ Code/main.c
@@ -1,20 +1,31 @@
 #include <stm32g071xx.h>
 
 #include "libsys.h"
+#include "libi2c.h"
 
-#include "libprotocol.h"
+#include "libpacket.h"
+
+#define I2C_ADDR_LTR390 0xA6
+
+
+int send = 0; 
+unsigned int meas_uv; 
+unsigned int meas_vis; 
+
+void test(void); 
+void test_send_packet(void); 
 
 void delayms(int ms) {
 	SysTick->CTRL = 0x4; 
 	SysTick->LOAD = 999; 
 	SysTick->VAL = 0x0; 
 	SysTick->CTRL = 0x5; 
-	for(int i = 0; i < ms; i++) 
+	for(int i = 0; i < ms; i++) {
 		while(!(SysTick->CTRL & 0x10000)); 
+//		test(); 
+	}
 	SysTick->CTRL = 0x4; 
 }
-
-void test(void); 
 
 int main(void) {
 	Sys_Init(); 
@@ -26,38 +37,103 @@ int main(void) {
 	GPIOB->BSRR = 1; 
 	
 	LPUART1_Init(); 
-	
-	test(); 
+	I2C_HWInit(); 
 	
 	while(1) {
-		GPIOB->BRR = 1; 
-		delayms(100); 
-		GPIOB->BSRR = 1; 
-		delayms(1900); 
+		delayms(1000); 
+		test_send_packet(); 
 	}
 }
 
-void test(void) {
-	Protocol_Init(); 
-	
-	unsigned char packetbuf[255]; 
-	Packet_t packet; 
-	packet.payload = packetbuf; 
-	packet.dir = 1; 
-	packet.pid = 1; 
-	packet.len = 3; 
-	packet.payload[0] = 0x61; 
-	packet.payload[1] = 0x62; 
-	packet.payload[2] = 0x63; 
-	
-	Protocol_TxString("AT+OK?"); 
-	Protocol_TxPacket(&packet); 
-	
-	for(;;) {
-		int x = Protocol_GetTx(); 
-		volatile char ch = x; 
-		if(x < 0) {
-			break; 
-		}
-	}
+
+void I2C_WriteSingleRegister(unsigned char devaddr, unsigned char regaddr, unsigned char data) {
+	unsigned char buf[1]; 
+	while(I2C_State.busy); 
+	I2C_Data.devaddr = devaddr; 
+	I2C_Data.oplen = 1; 
+	I2C_Data.opcode = regaddr; 
+	I2C_Data.wrlen = 1; 
+	I2C_Data.wrbuf = buf; 
+	I2C_Data.rdlen = 0; 
+	buf[0] = data; 
+	I2C_Session(); 
+	while(I2C_State.busy); 
 }
+
+unsigned int I2C_ReadRegisters3(unsigned char devaddr, unsigned char regaddr) {
+	unsigned char buf[3]; 
+	while(I2C_State.busy); 
+	I2C_Data.devaddr = devaddr; 
+	I2C_Data.oplen = 1; 
+	I2C_Data.opcode = regaddr; 
+	I2C_Data.wrlen = 0; 
+	I2C_Data.rdlen = 3; 
+	I2C_Data.rdbuf = buf; 
+	I2C_Session(); 
+	while(I2C_State.busy); 
+	unsigned int data = 0; 
+	data |= buf[0]; 
+	data |= buf[1] << 8; 
+	data |= buf[2] << 16; 
+	return data; 
+}
+
+unsigned int LTR390_Meas_UV(void) {
+	I2C_WriteSingleRegister(I2C_ADDR_LTR390, 0x04, 0x26); // 18bit, 2000ms
+	I2C_WriteSingleRegister(I2C_ADDR_LTR390, 0x05, 0x01); // 3x
+	I2C_WriteSingleRegister(I2C_ADDR_LTR390, 0x00, 0x0A); // UV measurement
+	delayms(120); 
+	I2C_WriteSingleRegister(I2C_ADDR_LTR390, 0x00, 0x00); // Stop
+	return I2C_ReadRegisters3(I2C_ADDR_LTR390, 0x10); 	
+}
+
+unsigned int LTR390_Meas_VIS(void) {
+	I2C_WriteSingleRegister(I2C_ADDR_LTR390, 0x04, 0x26); // 18bit, 2000ms
+	I2C_WriteSingleRegister(I2C_ADDR_LTR390, 0x05, 0x01); // 3x
+	I2C_WriteSingleRegister(I2C_ADDR_LTR390, 0x00, 0x02); // VIS measurement
+	delayms(120); 
+	I2C_WriteSingleRegister(I2C_ADDR_LTR390, 0x00, 0x00); // Stop
+	return I2C_ReadRegisters3(I2C_ADDR_LTR390, 0x0D); 	
+}
+
+
+void test(void) {
+	if(send) {
+		test_send_packet(); 
+		send = 0; 
+	}	
+}
+
+int serial_consumer(void * context, unsigned char data); 
+void test_send_packet(void) {
+	GPIOB->BRR = 1; 
+	meas_uv = LTR390_Meas_UV(); 
+	meas_vis = LTR390_Meas_VIS(); 
+	GPIOB->BSRR = 1; 
+	
+	unsigned char buffer[6]; 
+	Packet_t packet; 
+	packet.dir = 1; 
+	packet.pid = 0x20; 
+	packet.len = 6; 
+	packet.payload = buffer; 
+	
+	buffer[0] = meas_vis >> 16; 
+	buffer[1] = meas_vis >> 8; 
+	buffer[2] = meas_vis; 
+	buffer[3] = meas_uv >> 16; 
+	buffer[4] = meas_uv >> 8; 
+	buffer[5] = meas_uv; 
+	
+	serial_consumer(0, '#'); 
+	Packet_Encode(&packet, serial_consumer, 0); 
+	serial_consumer(0, '\r'); 
+	serial_consumer(0, '\n'); 
+}
+
+int serial_consumer(void * context, unsigned char data) {
+	while(!(LPUART1->ISR & USART_ISR_TXE_TXFNF)); 
+	LPUART1->TDR = data; 
+	return 0; 
+}
+
---
+++ Library/libi2c.c
@@ -0,0 +1,106 @@
+// The library is adapted from STM32G030
+#include <stm32g071xx.h>
+
+#include "libi2c.h"
+
+volatile I2C_Data_TypeDef I2C_Data; 
+volatile I2C_State_TypeDef I2C_State;
+
+unsigned int profile_irq_i2c1_cnt = 0; 
+
+void I2C_HWInit(void) {
+	I2C_State.busy = 0; 
+	// PB7: SDA (AF6)
+	// PB8: SCL (AF6)
+	RCC->IOPENR |= RCC_IOPENR_GPIOBEN; 
+	GPIOB->BSRR = 0x0180; 
+	GPIOB->AFR[1] = GPIOB->AFR[1] & 0xFFFFFFF0 | 0x00000006; 
+	GPIOB->AFR[0] = GPIOB->AFR[0] & 0x0FFFFFFF | 0x60000000; 
+	GPIOB->OTYPER |= 0x0180; 
+	GPIOB->MODER = GPIOB->MODER & 0xFFFC3FFF | 0x00028000; 
+	
+	// I2C1@1MHz, 100kHz
+	RCC->CCIPR = RCC->CCIPR & 0xFFFFCFFF; 
+	RCC->APBENR1 |= RCC_APBENR1_I2C1EN; 
+	__DSB(); 
+	I2C1->CR1 = 0x0; 
+	// Sample data from STM32126 @1MHz
+	I2C1->TIMINGR = 0x00000103; 
+//I2C1->TIMINGR = 0x30420F13; // 100kHz @16MHz
+//I2C1->TIMINGR = 0x10320309; // 400kHz @16MHz
+	// Enabled IRQ: TX/RXIE, NACKIE, TCIE, ERRIE, STOPF. DNF OFF, ANF ON. 
+	I2C1->CR1 = 0x000000F7; 
+	I2C1->ICR = 0x3F38; 
+	
+	// I2C IRQ Enable
+	NVIC_ClearPendingIRQ(I2C1_IRQn); 
+	NVIC_SetPriority(I2C1_IRQn, 3); 
+	NVIC_EnableIRQ(I2C1_IRQn); 
+}
+
+#define ctrl_wr_noend(addr, N) 		((addr) & 0xFE) | 0x00002000 | (((N) & 0xFF) << 16)
+#define ctrl_wr_autoend(addr, N) 	((addr) & 0xFE) | 0x02002000 | (((N) & 0xFF) << 16)
+#define ctrl_rd_autoend(addr, N) 	((addr) & 0xFE) | 0x02002400 | (((N) & 0xFF) << 16)
+
+int I2C_Session(void) {
+	if(I2C_State.busy) return 0; 
+	I2C_State.busy = 1; 
+	I2C_State.index = 0; 
+	I2C_State.error = 0; 
+	unsigned char devaddr = I2C_Data.devaddr; 
+	unsigned char effwrlen = I2C_Data.oplen + I2C_Data.wrlen; 
+	unsigned char effrdlen = I2C_Data.rdlen; 
+	if(effwrlen != 0) {
+		if(I2C_Data.rdlen == 0) 
+			I2C1->CR2 = ctrl_wr_autoend(devaddr, effwrlen); 
+		else 
+			I2C1->CR2 = ctrl_wr_noend(devaddr, effwrlen); 
+	}
+	else {
+		I2C1->CR2 = ctrl_rd_autoend(devaddr, effrdlen); 
+	}
+	return 1; 
+}
+
+void I2C1_IRQHandler(void) {
+	profile_irq_i2c1_cnt++; 
+	// Clear OVR/UDR/PEC/TIMEOUT/ALERT error (irrelevant error)
+	// Clear BERR (errata)
+	// Only error left is ARLO
+	I2C1->ICR = 0x3D00; 
+	unsigned int flags = I2C1->ISR; 
+	if(flags & 0x0200) { // ARLO
+		I2C1->ICR = 0x0200; 
+		I2C_State.error = I2C_ERR_ARLO; 
+	}
+	if(flags & 0x0010) { // NACK
+		I2C1->ICR = 0x0010; 
+		I2C_State.error = I2C_ERR_NACK; 
+	}
+	if(flags & 0x0020) { // STOPF
+		I2C1->ICR = 0x0020; 
+		if(I2C_State.busy) 
+			I2C_State.busy = 0; 
+	}
+	if(flags & 0x0040) { // TC (TCR not used)
+		// Only WR->RD stage comes here
+		I2C_State.index = 0; 
+		unsigned char devaddr = I2C_Data.devaddr; 
+		unsigned char effrdlen = I2C_Data.rdlen; 
+		I2C1->CR2 = ctrl_rd_autoend(devaddr, effrdlen); 
+	}
+	if(flags & 0x0002) { // TXIS
+		int index = I2C_State.index++; 
+		int oplen = I2C_Data.oplen; 
+		if(index < oplen) {
+			I2C1->TXDR = 0xFF & (I2C_Data.opcode >> ((oplen - index - 1) << 3)); 
+		}
+		else {
+			I2C1->TXDR = I2C_Data.wrbuf[index - oplen]; 
+		}
+	}
+	if(flags & 0x0004) { // RXNE
+		int index = I2C_State.index++; 
+		I2C_Data.rdbuf[index] = I2C1->RXDR; 
+	}
+}
