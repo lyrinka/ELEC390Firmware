bbfb6b210c1009b2e59000e8fa53d319e2bc288c
Lyrinka Hyacinthoides <47382398+lyrinka@users.noreply.github.com>
2023-07-16 13:06:14 +0800

    Roughly usable product

--- Code/main.c
+++ Code/main.c
@@ -27,11 +27,21 @@ void delayms(int ms) {
 	SysTick->CTRL = 0x4; 
 }
 
+int long_press_counter = 0; 
+void shutdown(void); 
+
 int main(void) {
 	Sys_Init(); 
 	
 	GPIOB->BRR = 1; 
-	GPIOB->MODER = GPIOB->MODER & 0xFFFFFFFC | 0x00000001; 
+	GPIOB->BSRR = 0x1000; 
+	GPIOB->OTYPER |= 0x1000; 
+	GPIOB->MODER = GPIOB->MODER & 0xC0FFFFFC | 0x01000001; 
+	GPIOB->PUPDR = GPIOB->PUPDR & 0xC3FFFFFF | 0x14000000; 
+	
+	GPIOA->BSRR = 0x00C0; 
+	GPIOA->PUPDR = GPIOA->PUPDR & 0xFFFFFFF0 | 0x00000005; 
+	GPIOA->MODER = GPIOA->MODER & 0xFFFF0FF0 | 0x00005000; 
 	
 	while(!Sys_LSEReady()); 
 	GPIOB->BSRR = 1; 
@@ -40,7 +50,30 @@ int main(void) {
 	I2C_HWInit(); 
 	
 	while(1) {
-		delayms(1000); 
+		delayms(300); 
+		
+		if(!(GPIOB->IDR & 0x2000)) // CHRG
+			GPIOA->BRR = 0x0080; 
+		delayms(40); 
+		GPIOA->BSRR = 0x0080; 
+		delayms(160); 
+		
+		if(!(GPIOB->IDR & 0x4000)) // FULL
+			GPIOA->BRR = 0x0040; 
+		delayms(40); 
+		GPIOA->BSRR = 0x0040; 
+		delayms(160); 
+		
+		if(!(GPIOA->IDR & 0x0001)) { // BTN MAIN
+			long_press_counter++; 
+			if(long_press_counter > 4) 
+				shutdown(); 
+		}
+		else {
+			long_press_counter = 0; 
+		}
+		
+		delayms(300); 
 		test_send_packet(); 
 	}
 }
@@ -82,7 +115,7 @@ unsigned int LTR390_Meas_UV(void) {
 	I2C_WriteSingleRegister(I2C_ADDR_LTR390, 0x04, 0x26); // 18bit, 2000ms
 	I2C_WriteSingleRegister(I2C_ADDR_LTR390, 0x05, 0x01); // 3x
 	I2C_WriteSingleRegister(I2C_ADDR_LTR390, 0x00, 0x0A); // UV measurement
-	delayms(120); 
+	delayms(110); 
 	I2C_WriteSingleRegister(I2C_ADDR_LTR390, 0x00, 0x00); // Stop
 	return I2C_ReadRegisters3(I2C_ADDR_LTR390, 0x10); 	
 }
@@ -91,7 +124,7 @@ unsigned int LTR390_Meas_VIS(void) {
 	I2C_WriteSingleRegister(I2C_ADDR_LTR390, 0x04, 0x26); // 18bit, 2000ms
 	I2C_WriteSingleRegister(I2C_ADDR_LTR390, 0x05, 0x01); // 3x
 	I2C_WriteSingleRegister(I2C_ADDR_LTR390, 0x00, 0x02); // VIS measurement
-	delayms(120); 
+	delayms(110); 
 	I2C_WriteSingleRegister(I2C_ADDR_LTR390, 0x00, 0x00); // Stop
 	return I2C_ReadRegisters3(I2C_ADDR_LTR390, 0x0D); 	
 }
@@ -135,5 +168,41 @@ int serial_consumer(void * context, unsigned char data) {
 	while(!(LPUART1->ISR & USART_ISR_TXE_TXFNF)); 
 	LPUART1->TDR = data; 
 	return 0; 
+}
+
+void shutdown(void) {
+	// Disable power to sensor and BLE
+	GPIOB->BRR = 0x0200; 
+	GPIOF->BRR = 0x0003; 
+	// Flash LED
+	delayms(500); 
+	for(int i = 0; i < 5; i++) {
+		GPIOA->BRR = 0x0040; 
+		delayms(50); 
+		GPIOA->BSRR = 0x0040; 
+		delayms(200); 
+	}
+	// Configuring IO ports
+	PWR->PUCRA = 0x00C0; 
+	PWR->PDCRA = 0x0000; 
+	PWR->PUCRB = 0x0001; 
+	PWR->PDCRB = 0x0200; 
+	PWR->PUCRC = 0x0000; 
+	PWR->PDCRC = 0x0000; 
+	PWR->PUCRD = 0x0000; 
+	PWR->PDCRD = 0x0000; 
+	PWR->PUCRF = 0x0000; 
+	PWR->PDCRF = 0x0003; 
+	// Enter shutdown mode
+	PWR->CR1 |= 0x4; 
+	SCB->SCR |= 0x40; 
+	__disable_irq(); 
+	while(1) {
+		__WFI(); 
+		GPIOB->BRR = 0x1; 
+		delayms(1000); 
+		GPIOB->BSRR = 0x1; 
+		delayms(100); 
+	}
 }
 
