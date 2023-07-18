4eb043dcde270def482ce8ed9a400678305e5d6d
Lyrinka Hyacinthoides <47382398+lyrinka@users.noreply.github.com>
2023-07-19 04:12:28 +0800

    I2C driver implementation

--- Code/main.c
+++ Code/main.c
@@ -4,6 +4,116 @@
 #include "libhandler.h"
 #include "libtask.h"
 
+#include "libi2c.h"
+
+void delayms(int); 
+
+unsigned char DAQTaskStack[512]; 
+unsigned char RootHandlerBackedStorage[1024]; 
+
+
+Task_t DAQTask; 
+Handler_t RootHandler; 
+
+
+#define I2C_ADDR_LTR390 0xA6
+
+void I2C_WriteSingleRegister(unsigned char devaddr, unsigned char regaddr, unsigned char data) {
+	unsigned char buf[1]; 
+	while(I2C_State.busy); 
+	I2C_Data.devaddr = devaddr; 
+	I2C_Data.oplen = 1; 
+	I2C_Data.opcode = regaddr; 
+	I2C_Data.wrlen = 1; 
+	I2C_Data.wrbuf = buf; 
+	I2C_Data.rdlen = 0; 
+	buf[0] = data; 
+	I2C_Session(); 
+	Task_Yield(); 
+}
+
+unsigned int I2C_ReadRegisters3(unsigned char devaddr, unsigned char regaddr) {
+	unsigned char buf[3]; 
+	while(I2C_State.busy); 
+	I2C_Data.devaddr = devaddr; 
+	I2C_Data.oplen = 1; 
+	I2C_Data.opcode = regaddr; 
+	I2C_Data.wrlen = 0; 
+	I2C_Data.rdlen = 3; 
+	I2C_Data.rdbuf = buf; 
+	I2C_Session(); 
+	Task_Yield(); 
+	unsigned int data = 0; 
+	data |= buf[0]; 
+	data |= buf[1] << 8; 
+	data |= buf[2] << 16; 
+	return data; 
+}
+
+unsigned int LTR390_Meas_UV(void) {
+	I2C_WriteSingleRegister(I2C_ADDR_LTR390, 0x04, 0x26); // 18bit, 2000ms
+	I2C_WriteSingleRegister(I2C_ADDR_LTR390, 0x05, 0x01); // 3x
+	I2C_WriteSingleRegister(I2C_ADDR_LTR390, 0x00, 0x0A); // UV measurement
+	delayms(110); // TODO: how to handle this delay?
+	I2C_WriteSingleRegister(I2C_ADDR_LTR390, 0x00, 0x00); // Stop
+	return I2C_ReadRegisters3(I2C_ADDR_LTR390, 0x10); 	
+}
+
+unsigned int LTR390_Meas_VIS(void) {
+	I2C_WriteSingleRegister(I2C_ADDR_LTR390, 0x04, 0x26); // 18bit, 2000ms
+	I2C_WriteSingleRegister(I2C_ADDR_LTR390, 0x05, 0x01); // 3x
+	I2C_WriteSingleRegister(I2C_ADDR_LTR390, 0x00, 0x02); // VIS measurement
+	delayms(110); // TODO: how to handle this delay?
+	I2C_WriteSingleRegister(I2C_ADDR_LTR390, 0x00, 0x00); // Stop
+	return I2C_ReadRegisters3(I2C_ADDR_LTR390, 0x0D); 	
+}
+
+
+
+void callbackRunDAQ(Handler_t * handler, unsigned int param) {
+	Task_Dispatch(&DAQTask); 
+}
+
+void I2C_StateChangeCallback(int error) {
+	Handler_Post(&RootHandler, callbackRunDAQ, error); 
+}
+
+void DAQTaskFunc(Task_t * self) {
+	for(;;) {
+		unsigned int uvi = LTR390_Meas_UV(); 
+		unsigned int vis = LTR390_Meas_VIS(); 
+		__nop(); 
+	}
+}
+
+
+
+int main(void) {
+	Sys_Init(); 
+	I2C_HWInit(); 
+	LED_Blue_On(); 
+	while(!Sys_LSEReady()); 
+	LED_Blue_Off(); 
+	
+	Task_Init(); 
+	Task_InitializeTask(&DAQTask, DAQTaskStack, sizeof(DAQTaskStack), DAQTaskFunc); 
+	
+	Handler_Init(&RootHandler, RootHandlerBackedStorage, sizeof(RootHandlerBackedStorage)); 
+	
+	Handler_Post(&RootHandler, callbackRunDAQ, 0); 
+	
+	while(1) {
+		int status = Handler_Execute(&RootHandler); 
+		if(status == HANDLER_EXECUTOR_EMPTY) {
+			LED_Green_Off(); 
+			for(int i = 0; i < 100; i++); 
+		}
+		else LED_Green_On(); 
+	}
+}
+
+
+
 void delayms(int ms) {
 	SysTick->CTRL = 0x4; 
 	SysTick->LOAD = 999; 
@@ -12,91 +122,5 @@ void delayms(int ms) {
 	for(int i = 0; i < ms; i++) 
 		while(!(SysTick->CTRL & 0x10000)); 
 	SysTick->CTRL = 0x4; 
-}
-
-unsigned char stack2[512]; 
-unsigned char stack3[512]; 
-unsigned char memory1[1024]; 
-
-
-Task_t task2; 
-void task2_func(Task_t * self); 
-
-Task_t task3; 
-void task3_func(Task_t * self); 
-
-void callback1(Handler_t *, unsigned int); 
-void callback2(Handler_t *, unsigned int); 
-void callback3(Handler_t *, unsigned int); 
-
-int main(void) {
-	Sys_Init(); 
-	
-	LED_Blue_On(); 
-	while(!Sys_LSEReady()); 
-	LED_Blue_Off(); 
-	
-	Task_Init(); 
-	
-	Task_InitializeTask(&task2, stack2, sizeof(stack2), task2_func); 
-	Task_InitializeTask(&task3, stack3, sizeof(stack3), task3_func); 
-	
-	Handler_t rootHandler; 
-	
-	Handler_Init(&rootHandler, memory1, sizeof(memory1)); 
-	Handler_Post(&rootHandler, callback1, 0); 
-	
-	while(1) {
-		int status = Handler_Execute(&rootHandler); 
-		if(status == HANDLER_EXECUTOR_EMPTY) {
-			LED_Green_On(); 
-			for(int i = 0; i < 100; i++); 
-		}
-		else LED_Green_Off(); 
-	}
-}
-
-void callback1(Handler_t * handler, unsigned int param) {
-	LED_Blue_On(); 
-	delayms(100); 
-	LED_Blue_Off(); 
-	delayms(400); 
-	Handler_Post(handler, callback2, param); 
-	Handler_Post(handler, callback2, param); 
-}
-
-void callback2(Handler_t * handler, unsigned int param) {
-	LED_Red_On(); 
-	delayms(100); 
-	LED_Red_Off(); 
-	delayms(400); 
-	Handler_Post(handler, callback3, param); 
-}
-
-
-void callback3(Handler_t * handler, unsigned int param) {
-	if(++param > 5) return; 
-	Handler_Post(handler, callback1, param); 
-}
-
-
-void task2_func(Task_t * self) {
-	while(1) {
-		LED_Red_On(); 
-		delayms(100); 
-		LED_Red_Off(); 
-		delayms(900); 
-		Task_Yield(); 
-	}
-}
-
-void task3_func(Task_t * self) {
-	while(1) {
-		LED_Green_On(); 
-		delayms(100); 
-		LED_Green_Off(); 
-		delayms(900); 
-		Task_Yield(); 
-	}
 }
 
---
+++ Library/libi2c.h
@@ -0,0 +1,30 @@
+#ifndef LIBI2C_H__
+#define LIBI2C_H__
+
+#define I2C_ERR_NOERR 0
+#define I2C_ERR_NACK 1
+#define I2C_ERR_ARLO 2
+
+typedef struct {
+	unsigned char devaddr; 
+	unsigned char oplen;
+	unsigned char wrlen; 
+	unsigned char rdlen; 
+	unsigned int opcode; 
+	const unsigned char * wrbuf; 
+	unsigned char * rdbuf; 
+} I2C_Data_TypeDef; 
+
+typedef struct {
+	unsigned char busy; 
+	unsigned char index; 
+	unsigned char error; 
+} I2C_State_TypeDef; 
+
+extern volatile I2C_Data_TypeDef I2C_Data; 
+extern volatile I2C_State_TypeDef I2C_State; 
+
+extern void I2C_HWInit(void); 
+extern int I2C_Session(void); 
+
+#endif
